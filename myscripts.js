895a5244b386bd94a6834e76af65c5c268ecdf60
Prateek gupta <guptaprateek652@gmail.com>
2020-08-26 15:34:37 +0530

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,7 @@
+window.onload = () => {
+  alert('HOW TO GET STARTED --->\n\nThis is a Sticky Note Web app where you can create your important notes and also delete them.\n This web app is installable on your mobile and computer, you just need to Follow these steps:-\n1. Click on three vertical dots on top right corner of browser.\n2. Click on Add To Home screen Option.\n3. Select ADD option.\nBOOM! YOU JUST INSTALLED THE APPICATION.\n\nThis web app also has a feature of offline operation i.e(no internet usage) and your data always remain saved, you can modify as per your needs.\n\n-Prateek Gupta');
+}
+
 if ("serviceWorker" in navigator) {
     // register service worker
     navigator.serviceWorker.register("service-worker.js");
