405178305ebac3c84a3f2b7dca301c9bb95de39a
Prateek gupta <guptaprateek652@gmail.com>
2020-08-26 19:05:04 +0530

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   if (localStorage.getItem("hasCodeRunBefore") === null) {
-      /** Your code here **/
+      /** Your code here*/
       alert('HOW TO GET STARTED --->\n\nThis is a Sticky Note Web app where you can create your important notes and also delete them.\n\n This web app is installable on your mobile and computer, you just need to Follow these steps:-\n1. Click on three vertical dots on top right corner of browser.\n2. Click on Add To Home screen Option.\n3. Select ADD option.\nBOOM! YOU JUST INSTALLED THE APPICATION.\n\nThis web app also has a feature of offline operation i.e(no internet usage) and your data always remain saved, you can modify as per your needs.\n\n-Prateek Gupta');
 
       localStorage.setItem("hasCodeRunBefore", true);
@@ -24,7 +24,7 @@ if ("serviceWorker" in navigator) {
 
   
   let createNote = (noteTitle, noteBody, dateStamp) => {
-    if (count > -1) {
+    if (count > 0) {
       document.getElementById("no-notes").className = "hidden";
     }
   
